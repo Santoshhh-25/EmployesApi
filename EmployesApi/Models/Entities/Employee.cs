f05ba0547c883680e5c732afdb9761f25350ee51
Santoshh-25 <santoshdhamala.lp1190@gmail.com>
2025-12-07 12:40:13 +0530

    Add project files.

---
+++ EmployesApi/Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployesApi.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
